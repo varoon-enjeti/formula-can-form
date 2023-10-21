2c6da0227de20a84a499f11c901974db73b7ed55
varoon-enjeti <varoon.enjeti@gmail.com>
2023-10-21 12:58:24 -0500

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      <div className="text-4xl text-purple-800 font-medium">Northwestern Formula CAN Request</div>
+      <form action="" className="">
+        
+      </form>
+    </main>
+  )
+}
